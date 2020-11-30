b639cd5d247a01fca6f27f8f9b180c6914ad3c74
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2020-11-30 13:17:11 +0530

    Create 3 Forward List STL .cpp

---
+++ 4. Lists/3 Forward List STL .cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<list> //header file for List
+using namespace std;
+
+// Forward List: it keeps internally only the link to the next element (not like List / Double Linked List, which keeps two links per elements)
+// Fwd List are more efficient bcoz they will take less memory
+
+// read more on : http://www.cplusplus.com/reference/forward_list/forward_list/
+

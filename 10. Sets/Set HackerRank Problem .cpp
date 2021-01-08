ed692a3bbff2504763cd283b429009e302051696
Tцѕндя Kцкяд <43209472+MaverickTK@users.noreply.github.com>
2021-01-08 23:41:07 +0530

    Update Set HackerRank Problem .cpp

--- 10. Sets/Set HackerRank Problem .cpp
+++ 10. Sets/Set HackerRank Problem .cpp
@@ -1,4 +1,4 @@
-Problem Statement: 
+Problem Statement: (link : https://www.hackerrank.com/challenges/cpp-sets/problem )
 
 Sets are a part of the C++ STL. Sets are containers that store unique elements following a specific order. Here are some of the frequently used member functions of sets:
 
@@ -56,3 +56,47 @@ Sample Output
 Yes
 No
 No
+
+
+Code: 
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    set <int> s;
+    int N = 0;
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        int q = 0, x = 0;
+        cin >> q;
+        cin >> x;
+        if (q == 1)
+            s.insert(x);
+        else if (q == 2)
+            s.erase(x);
+        else
+        {
+            auto itr=s.find(x);
+            if (itr == s.end())
+                cout<<"No"<<endl;
+            else
+                cout<<"Yes"<<endl;
+        }
+    }
+    return 0;
+}
+
+
+
+                    
+
+              
